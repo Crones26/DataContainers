9cecc84719408202e0670d467680e8d343ac9f1e
Devya_yi23@top-academy.ru <Devya_yi23@top-academy.ru>
2024-08-26 21:06:16 +0500

    HomeWork DONE

--- List2/main.cpp
+++ List2/main.cpp
@@ -4,7 +4,159 @@ using std::cout;
 using std::endl;
 
 #define tab "\t"
+#define delimiter "\n-------------------------------------------------\n"
 
+class List
+{
+	class Element
+	{
+		int Data;
+		Element* pNext;
+		Element* pPrev;
+	public:
+		Element(int Data, Element* pNext = nullptr, Element* pPrev = nullptr)
+			:Data(Data), pNext(pNext), pPrev(pPrev)
+		{
+			cout << "EConstructor:\t" << this << endl;
+		}
+		~Element()
+		{
+			cout << "EDestructor:\t" << this << endl;
+		}
+		friend class List;
+	}*Head, * Tail;
+	size_t size;
+public:
+	List()
+	{
+		Head = Tail = nullptr;
+		size = 0;
+		cout << "LConstructor:\t" << this << endl;
+	}
+	~List()
+	{
+		//while (Head)pop_front();
+		while (Tail)pop_back();
+		cout << "LDestructor:\t" << this << endl;
+	}
+
+	//				Adding elements:
+	void push_front(int Data)
+	{
+		if (Head == nullptr && Tail == nullptr)
+		{
+			Head = Tail = new Element(Data);
+		}
+		else
+		{
+			Element* New = new Element(Data);	//1)
+			New->pNext = Head;	//2)
+			Head->pPrev = New;	//3)
+			Head = New;	//4) 
+		}
+		size++;
+	}
+	void push_back(int Data)
+	{
+		if (Head == nullptr && Tail == nullptr)
+		{
+			Head = Tail = new Element(Data);
+		}
+		else
+		{
+			Element* New = new Element(Data);//1)
+			New->pPrev = Tail;//2)
+			Tail->pNext = New;//3)
+			Tail = New;//4)
+		}
+		size++;
+	}
+
+	//				Removing elements:
+	void pop_front()
+	{
+		if (Head == nullptr && Tail == nullptr)return;
+		if (Head == Tail)
+		{
+			delete Head;
+			Head = Tail = nullptr;
+		}
+		else
+		{
+			Head = Head->pNext;		//1)
+			delete Head->pPrev;		//2)
+			Head->pPrev = nullptr;	//3) 
+		}
+		size--;
+	}
+	void pop_back()
+	{
+		if (Head == nullptr && Tail == nullptr)return;
+		if (Head == Tail)
+		{
+			delete Tail;
+			Head = Tail = nullptr;
+		}
+		else
+		{
+			Tail = Tail->pPrev;		//1)
+			delete Tail->pNext;		//2)
+			Tail->pNext = nullptr;	//3)
+		}
+		size--;
+	}
+
+	//				Methods:
+	void print()const
+	{
+		cout << delimiter << endl;
+		cout << "Head:\t" << Head << endl;
+
+		for (Element* Temp = Head; Temp; Temp = Temp->pNext)
+			cout
+			<< Temp->pPrev << tab
+			<< Temp << tab
+			<< Temp->Data << tab
+			<< Temp->pNext << endl;
+
+		cout << "Tail:\t" << Tail << endl;
+		cout << "?????????? ????????? ??????: " << size << endl;
+		cout << delimiter << endl;
+	}
+	void reverse_print()const
+	{
+		cout << delimiter << endl;
+		cout << "Tail:\t" << Tail << endl;
+
+		for (Element* Temp = Tail; Temp; Temp = Temp->pPrev)
+			cout
+			<< Temp->pPrev << tab
+			<< Temp << tab
+			<< Temp->Data << tab
+			<< Temp->pNext << endl;
+
+		cout << "Head:\t" << Head << endl;
+		cout << "?????????? ????????? ??????: " << size << endl;
+		cout << delimiter << endl;
+	}
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n;
+	cout << "??????? ?????????? ?????????: "; cin >> n;
+	List list;
+	for (int i = 0; i < n; i++)
+	{
+		//list.push_front(rand() % 100);
+		list.push_back(rand() % 100);
+	}
+	list.print();
+	list.reverse_print();
+	for (int i = 0; i < 100; i++)list.pop_back();
+	list.reverse_print();
+}
 void main()
 {
 	setlocale(LC_ALL, "");
