005c0e06127ff91568e3c37b3744c1f9c56324c1
Devya_yi23@top-academy.ru <Devya_yi23@top-academy.ru>
2024-09-01 01:16:12 +0500

    TemplatedList2 Home Work DONE

--- List2/main.cpp
+++ List2/main.cpp
@@ -403,7 +403,10 @@ template<typename T> void Grow(List<T>& list)
 
 //#define BASE_CHECK
 //#define ITERATORS_CHECK
-#define OPERATOR_PLUS_CHECK
+//#define OPERATOR_PLUS_CHECK
+//#define DOUBLE_CHEK
+#define STD_STRING_CHEK
+
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -456,5 +459,43 @@ void main()
 	for (int i : list1)cout << i << tab; cout << endl;
 
 #endif // OPERATOR_PLUS_CHECK
+
+#ifdef DOUBLE_CHEK
+	//				Double:
+	List<double> d_list = { 2.7, 3.14, 5.5, 8.3 };
+	for (double i : d_list)cout << i << tab; cout << endl;
+	for (List<double>::Iterator it = d_list.begin(); it != d_list.end(); ++it)
+		cout << *it << tab; cout << endl;
+	for (List<double>::ReverseIterator it = d_list.rbegin(); it != d_list.rend(); ++it)
+		cout << *it << tab; cout << endl;
+	cout << delimiter << endl;
+
+	/*List<double> d_list1 = { 3.14, 2.71, 1.41, 1.73 };
+	List<double> d_list2 = { 6.28, 3.14 };
+	List<double> d_list3 = d_list1 + d_list2;
+	for (double d : d_list3) cout << d << tab;
+	cout << endl;
+
+	Grow(double_list3);
+	for (double d : d_list3) cout << d << tab;
+	cout << endl << endl;*/
+#endif // DOUBLE_CHEK
+
+#ifdef STD_STRING_CHEK
+	//				std::string:
+	List<std::string> s_list = { "Hello", "World" ,"Templates" ,"DONE" };
+	for (std::string i : s_list)cout << i << tab; cout << endl;
+	for (List<std::string>::Iterator it = s_list.begin(); it != s_list.end(); ++it)
+		cout << *it << tab; cout << endl;
+	for (List<std::string>::ReverseIterator it = s_list.rbegin(); it != s_list.rend(); ++it)
+		cout << *it << tab; cout << endl;
+	cout << delimiter << endl;
+
+	/*List<std::string> string_list1 = { "Hello", "World","Templates" };
+	List<std::string> string_list2 = {"Powerful", "DONE"};
+	List<std::string> string_list3 = string_list1 + string_list2;
+	for (const std::string& s : string_list3) cout << s << tab;
+	cout << endl;*/
+#endif // STD_STRING_CHEK
 
 }
